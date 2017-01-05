de3be9d7af90a8b35fdb382064e2b9205e82fba3
Joshua Wulf <sitapati@worldsankirtan.net>
2017-01-06 05:34:23 +1000

    Change to official Slack SDK

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
 const gelfserver = require('graygelf/server')
-const Slack = require('slack-node');
-const apiToken = process.env.SLACK_API_TOKEN;
 const server = gelfserver();
 
-slack = new Slack(apiToken);
+var RtmClient = require('@slack/client').RtmClient;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var bot_token = process.env.SLACK_API_TOKEN || '';
+
+let init = false;
+var rtm = new RtmClient(bot_token);
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED,  (rtmStartData) => {
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+});
+
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
+  rtm.sendMessage("Hello!", channel);
+  init = true;
+});
+
+rtm.start();
+
 var channels = {};
 
 const short = (long) => { return long.split('.')[0] }
 
-const joinChannel = (name, cb) => { slack.api('channels.join', { name }, cb) }
-
-slack.on('channel_joined', (evt) => {
+rtm.on('channel_joined', (evt) => {
 	channels[evt.channel] = true;
 })
 
 server.on('message', (gelf) => {
-	if (!gelf || !gelf.host) return;
+	if (!gelf || !gelf.host || !init) return;
 	const name = short(gelf.host);
 	if (channels[name]) { return announce(`#${name}`, gelf) }
 	announce('#general', gelf);
-	if (channels[name] !== false) { joinChannel(name), (res) => {
-		channels[name] = channels[name] || (res && res.ok)
-	}
-}});
+});
 
 server.listen(12201);
 
 const announce = (channel, msg) => { 
-	const send = (text) => {
-		slack.api('chat.postMessage', { text, channel }, (err, res) => { console.log(res) });
-		console.log(text);
-	}
+	const send = (text) => { rtm.sendMessage(text, channel) }
 	if (msg && msg.short_message) { send(msg.shortmessage) }
 	if (msg && msg.full_message)  { send(msg.full_message) }
 }
