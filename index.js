95855fa3dd182106e3b38627f7d52075c28e9b8e
Joshua Wulf <sitapati@worldsankirtan.net>
2017-01-06 04:02:26 +1000

    Next-gen JavaScript

--- index.js
+++ index.js
@@ -7,30 +7,33 @@ slack = new Slack(apiToken);
 var channels = {};
 
 server.on('message', (gelf) => {
-	console.log('received message', gelf.short_message);
+	console.log('received message', gelf);
 	if (!channels[gelf.host]) {
 		slack.api('channels.join', { name: gelf.host }, (res) => {
-			if (res && !res.ok) { return sendToChannel('general', gelf); }
+			if (res && !res.ok) { return announce('#general', gelf); }
 			channels[gelf.host] = res;
-			sendToChannel('general', `New Channel: ${gelf.host}`)
-			return sendToChannel(gelf.host, gelf);
+			announce('#general', `New Channel: ${gelf.host}`)
+			return announce(`#${gelf.host}`, gelf);
 		});
 		return;
 	}
-	sendToChannel(gelf.host, gelf);
+	announce(`#${gelf.host}`, gelf);
 });
 
 server.listen(12201);
 
-function sendToChannel(channel, msg) {
-	function send(txt) {
-		slack.api('chat.postMessage', {
-			text: txt,
-			channel: `#${channel}`
-		}, (err, response) => { console.log(response); });
+const joinChannel = (channel, cb) => { slack.api('channels.join', { name: channel }, cb) }
+
+const announce = (channel, msg) => { 
+	const send = (text) => {
+		slack.api('chat.postMessage', { text, channel }, (err, res) => { console.log(res); });
+		console.log(text);
 	}
-	(gelf && gelf.short_message && true === send(gelf.shortmessage))
-	(gelf && gelf.full_message && true === send(gelf.full_message));
+	("undefined" != typeof msg && msg.short_message && true === send(msg.shortmessage))
+	("undefined" != typeof msg && msg.full_message && true === send(msg.full_message));
 }
 
-console.log('A wizard is never late. He arrives precisely when he means to!');
+announce(
+	'#general',
+	'A wizard is never late. He arrives precisely when he means to!'
+);
