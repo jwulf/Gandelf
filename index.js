b5e3f61dbfb4f54ad35dbe9c847c42af2f936e2c
Joshua Wulf <sitapati@worldsankirtan.net>
2017-01-06 11:23:38 +1000

    Slack API issue #30

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const gelfserver = require('graygelf/server')
 const server = gelfserver();
 var Bottleneck = require("bottleneck"); 
-var RtmClient = require('@slack/client').RtmClient;
-var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
 var bot_token = process.env.SLACK_API_TOKEN || '';
 
-var init = false;
+let init = false;
+let general;
+
+var RtmClient = require('@slack/client').RtmClient;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var rtm = new RtmClient(bot_token);
+
 var limiter = new Bottleneck(1, 800);
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED,  (rtmStartData) => {
+  sd = rtmStartData;
+  for (const c of sd.channels) {
+	  if (c.is_member) {
+		  channels[c.name] = c.id;
+		  if (name === 'general') { general = c.id}
+		}
+  }
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
 
-rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
-	console.log('Yo');
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, (res) => {
 	init = true;
-  	rtm.sendMessage('#general', "Hello!",  (err, res) => { console.log(err); });
+  	rtm.sendMessage(general, "Hello!",  (err, res) => { console.log(err); });
 });
 
 rtm.start();
@@ -27,20 +37,21 @@ const short = (long) => { return long.split('.')[0] }
 
 rtm.on('channel_joined', (evt) => {
 	console.log(evt);
-	channels[evt.channel] = true;
+	channels[evt.channel.name] = evt.channel.id;
 })
 
 const announce = (channel, msg) => { 
 	const send = (text) => { limiter.submit(rtm.sendMessage,text, channel, 
 		(err, res) => { if (err) { console.log(err) } }); }
-	if (msg && msg.short_message) { send(msg.shortmessage) }
+	if (msg && msg.short_message && channel) { send(msg.shortmessage) }
 }
 
 server.on('message', (gelf) => {
+	console.log(gelf)
 	if (!gelf || !gelf.host || !init) return;
 	const name = short(gelf.host);
 	if (channels[name]) { return announce(`#${name}`, gelf) }
-	announce('#general', name);
+	announce(general, name);
 });
 
 server.listen(12201);
