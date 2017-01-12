c0934d0a7931324e86d12b025c3136b3d2f43302
Joshua Wulf <sitapati@worldsankirtan.net>
2017-01-12 16:21:34 +1000

    fix seq

--- adapters/seq.js
+++ adapters/seq.js
@@ -1,21 +1,25 @@
 var structuredLog = require('structured-log');
 var seqSink = require('../lib/structured-log-seq-sink');
 
+'use strict';
+
+const SEQ_URL = process.env.SEQ_URL || 'http://127.0.0.1';
+
 var logger = structuredLog.configure()
   .writeTo(seqSink({
-    url: 'http://magik-seq.southeastasia.cloudapp.azure.com',
+    url: SEQ_URL,
     compact: true
 }))
   .create();
 
 console.log('Seq logging is enabled');
-logger.info('Seq logging is enabled');
+seqMessage({short_message: 'Seq logging is enabled', a: 1});
 
 function seqMessage(msg) {
     var shortMessage = msg.short_message || '<No message>';
-    var fields = {...msg};
+    var fields = Object.assign({}, msg);
     delete fields.short_message;
-    logger.enrich(msg).info(shortMessage);
+    logger.enrich(fields).info(shortMessage);
 }
 
 module.exports.seqMessage = seqMessage;
--- adapters/slack.js
+++ adapters/slack.js
@@ -4,54 +4,57 @@ var Bot = require('slackbots');
 var bot_token = process.env.SLACK_API_TOKEN || '';
 var rate_limit = process.env.RATE_LIMIT || 400; // 400 milliseconds between messages
 
-var settings = {
-    token: bot_token,
-    name: 'gandelf'
-};
+if (bot_token) {
+    var settings = {
+        token: bot_token,
+        name: 'gandelf'
+    };
 
-var bot = new Bot(settings);
+    var bot = new Bot(settings);
 
-var limiter = new Bottleneck(0, 400);
+    var limiter = new Bottleneck(0, 400);
 
-general = 'general'
-var channels = {};
+    general = 'general'
+    var channels = {};
 
-bot.on('channel_joined', (evt) => {
-	console.log('Channel joined');
-	console.log(evt);
-	channels[evt.channel.name] = evt.channel.name;
-})
+    bot.on('channel_joined', (evt) => {
+        console.log('Channel joined');
+        console.log(evt);
+        channels[evt.channel.name] = evt.channel.name;
+    })
 
-bot.on('start',  () => {
-	bot.getChannels().always((chans) => {
-		let chanlist = '';
-		for (const chan of chans._value.channels) {
-			if (chan.is_member) {
-				channels[chan.name] = chan.name;
-				chanlist += `${chan.name} `;
-			}
-		}
-		init = true;
-		announce('general', `Gandelf alive. Currently in these channels: ${chanlist}`);
-	});
-});
+    bot.on('start',  () => {
+        bot.getChannels().always((chans) => {
+            let chanlist = '';
+            for (const chan of chans._value.channels) {
+                if (chan.is_member) {
+                    channels[chan.name] = chan.name;
+                    chanlist += `${chan.name} `;
+                }
+            }
+            init = true;
+            announce('general', `Gandelf alive. Currently in these channels: ${chanlist}`);
+        });
+    });
 
-const announce = (channel, msg) => { 
-	const send = (text) => { 
-		console.log(text);
-		return new Promise( (resolve, reject) => {
-			bot.postMessageToChannel(channel, text).always((data) => resolve);
-		});
-	}
+    const announce = (channel, msg) => { 
+        const send = (text) => { 
+            console.log(text);
+            return new Promise( (resolve, reject) => {
+                bot.postMessageToChannel(channel, text).always((data) => resolve);
+            });
+        }
 
-	let txt;
-	if (typeof msg === "string") { txt = msg }
-	if (msg && msg.short_message) { txt = msg.short_message }
+        let txt;
+        if (typeof msg === "string") { txt = msg }
+        if (msg && msg.short_message) { txt = msg.short_message }
 
-	limiter.schedule(send, txt).then((data)=> console.log); 
+        limiter.schedule(send, txt).then((data)=> console.log); 
+    }
 }
 
 function slackMessage(msg, name) {
+    if (!bot_token) { return; }
 	if (channels[name]) { return announce(channels[name], msg) }
 	announce(general, `${name} - ${msg}`);
 }
