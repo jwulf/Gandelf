057f69dc1118fcb368d9ec4214a882fc95a56a7c
Joshua Wulf <sitapati@worldsankirtan.net>
2017-02-10 07:55:12 +1000

    Emit message count

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const slackMessage = require('./adapters/slack').slackMessage;
 const seqMessage = require('./adapters/seq').seqMessage;
 const azureMessage = require('./adapters/azure-msg-queue').azureMessage;
 let general = 'general';
+let msgCount = 0;
 
 const NAME = process.env.GELF_NAME || null;
 
 const short = (long) => { return long.split('.')[0] }
 
 server.on('message', (gelf) => {
+	msgCount += 1;
+	console.log(`Message Count: ${mgsCount}`);
 	if (!gelf || !gelf.host || !gelf.short_message) { return; }
 	const name = NAME || short(gelf.host);
 	slackMessage(gelf.short_message, name);
