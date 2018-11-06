8c34798cb2339fd3e9350c7572355034ac436d15
Jaben Cargman <jaben.cargman@captiveaire.com>
2018-11-06 15:50:54 -0500

    Fixes #4

--- adapters/local-echo.js
+++ adapters/local-echo.js
@@ -3,31 +3,31 @@ const chalk = require('chalk')
 const size = require('lodash.size')
 
 const { Local } = config || {}
-const { echo } = Local || {}
+let { echo } = Local || {}
 
 const colors = [
-	chalk.cyan,
-	chalk.yellow,
-	chalk.cyan,
-	chalk.blue,
-	chalk.magenta
+    chalk.cyan,
+    chalk.yellow,
+    chalk.cyan,
+    chalk.blue,
+    chalk.magenta
 ]
 
-const initialise = ({containers, init}) => ({containers, init: !!echo})
+const initialise = ({ containers, init }) => ({ containers, init: !!echo })
 
-const echo = ({containers, init}) => gelf => {
-	if (!init) { return }
-	const name = gelf._container_name || 'default'
-	containers[name] = containers[name] || colors[(size(containers) % colors.length)]
-	const colorised = containers[name] || chalk.white
-	console.log(colorised(`[${name}] - ${gelf.short_message}`))
+echo = ({ containers, init }) => gelf => {
+    if (!init) { return }
+    const name = gelf._container_name || 'default'
+    containers[name] = containers[name] || colors[(size(containers) % colors.length)]
+    const colorised = containers[name] || chalk.white
+    console.log(colorised(`[${name}] - ${gelf.short_message}`))
 }
 
 const LocalJSONLogging = chalk.yellow('Local JSON Logging: ')
 if (!echo) {
-	console.log(chalk.bold(LocalJSONLogging + chalk.red('Disabled')))
+    console.log(chalk.bold(LocalJSONLogging + chalk.red('Disabled')))
 } else {
-	console.log(chalk.bold(LocalJSONLogging + chalk.green('Enabled')))
+    console.log(chalk.bold(LocalJSONLogging + chalk.green('Enabled')))
 }
 
-module.exports = echo(initialise({containers: {}, init: false}))
+module.exports = echo(initialise({ containers: {}, init: false }))
